51f746a224773d5c4d56d3cc4d9a028697a8b981
wangminan <wangminan0811@hotmail.com>
2025-04-02 12:07:41 +0800

    【FE】项目结构搭建

---
+++ README.md
@@ -0,0 +1,3 @@
+## zorathos-analog-interface
+
+zorathos-analog-interface是中台为人机交互项目准备的模拟接口，基于zorathos-data-model构建
---
+++ src/main/java/org/datacenter/analog/Main.java
@@ -0,0 +1,10 @@
+package org.datacenter.analog;
+
+import io.vertx.core.AbstractVerticle;
+
+public class Main extends AbstractVerticle {
+
+    public static void main(String[] args) {
+
+    }
+}
